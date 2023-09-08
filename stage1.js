89e80d974c9d98c90c68ffe17c51a621609d261c
Damilola A.J <iamroot@kali.kali>
2023-09-08 09:53:09 +0100

    stage1

--- stage1.js
+++ stage1.js
@@ -5,20 +5,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/api', (req, res) => {
-  // Get query parameters from the request
   const { slack_name, track } = req.query;
 
-  // Get the current day of the week in full
   const currentDay = moment().utc().format('dddd');
 
-  // Get the current UTC time with validation of +/-2 minutes
   const currentUTCTime = moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]');
 
-  // Construct GitHub URLs
-  const githubFileURL = 'https://github.com/username/repo/blob/main/file_name.ext';
-  const githubRepoURL = 'https://github.com/username/repo';
+  const githubFileURL = 'https://github.com/itsralia/stage1/blob/main/stage1.js';
+  const githubRepoURL = 'https://github.com/itsralia/stage1';
 
-  // Prepare the JSON response
   const jsonResponse = {
     slack_name,
     current_day: currentDay,
