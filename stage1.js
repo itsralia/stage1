505e2a77395ca6731e28467524cb31abc0e74188
Damilola A.J <iamroot@kali.kali>
2023-09-08 10:21:38 +0100

    stage1

--- stage1.js
+++ stage1.js
@@ -7,17 +7,24 @@ const port = process.env.PORT || 3000;
 app.get('/api', (req, res) => {
   const { slack_name, track } = req.query;
 
-  const currentDay = moment().utc().format('dddd');
 
-  const currentUTCTime = moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]');
+  const currentUTCTime = moment().utc();
+  const minAllowedTime = moment().utc().subtract(2, 'minutes');
+  const maxAllowedTime = moment().utc().add(2, 'minutes');
+
+  if (currentUTCTime.isBefore(minAllowedTime) || currentUTCTime.isAfter(maxAllowedTime)) {
+    return res.status(500).json({ error: 'Sorry, UTC time is out of the allowed window' });
+  }
+
+  const formattedUTCTime = currentUTCTime.format('YYYY-MM-DDTHH:mm:ss[Z]');
 
   const githubFileURL = 'https://github.com/itsralia/stage1/blob/main/stage1.js';
   const githubRepoURL = 'https://github.com/itsralia/stage1';
 
   const jsonResponse = {
     slack_name,
-    current_day: currentDay,
-    utc_time: currentUTCTime,
+    current_day: currentUTCTime.format('dddd'),
+    utc_time: formattedUTCTime,
     track,
     github_file_url: githubFileURL,
     github_repo_url: githubRepoURL,
